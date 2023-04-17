7e65a45aa6c8ce7b525809d6f16b3d52953eb30c
robert <robert.hak@tul.cz>
2023-04-17 11:47:41 +0200

    added template for cv6 for python

---
+++ plain_python/cviceni/cv6/search.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+
+"""
+Úkol 6.
+Vaším dnešním úkolem je vytvořit program, který o zadaném textu zjistí některé
+údaje a vypíše je na standardní výstup. Hlavním smyslem cvičení je procvičit
+si práci s regulárními výrazy, takže pro plný bodový zisk je nutné použít k
+řešení právě tento nástroj.
+
+Program musí pracovat s obecným textem, který bude zadaný v souboru. Jméno
+souboru bude zadáno jako vstupní parametr funkce main, která by měla být
+vstupním bodem programu. Samozřejmě, že funkce main by neměla řešit problém
+kompletně a měli byste si vytvořit další pomocné funkce. Můžete předpokládat,
+že soubor bude mít vždy kódování utf-8 a že bude psaný anglicky, tedy jen
+pomocí ASCII písmen, bez české (či jiné) diakritiky.
+
+Konkrétně musí program zjistit a vypsat:
+
+1. Počet slov, která obsahují nejméně dvě samohlásky (aeiou) za sebou. Například
+slovo bear.
+
+2. Počet slov, která obsahují alespoň tři samohlásky - například slovo atomic.
+
+3. Počet slov, která mají šest a více znaků - například slovo terrible.
+
+4. Počet řádků, které obsahují nějaké slovo dvakrát.
+
+Podrobnější zadání včetně příkladu je jako obvykle na elearning.tul.cz
+"""
+
+
+def main(file_name):
+    """
+    zpracujte soubor
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main('simple.txt')
