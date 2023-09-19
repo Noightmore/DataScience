1e691c7db52838362011387b092268236c8a6a46
robert <robert.hak@tul.cz>
2023-09-19 19:28:14 +0200

    cv1 done

--- multimedia/cv1/cv1.py
+++ multimedia/cv1/cv1.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import librosa
 import librosa.display
+import struct
 
 
 def read_wav_header(wav_file):
@@ -12,21 +13,73 @@ def read_wav_header(wav_file):
         for key, value in wf.getparams()._asdict().items():
             print(f"{key.capitalize()}: {value}")
 
+    print("---------------------------------")
+
 
 def plot_wav_file(wav_file):
-    # Read audio data
-    with wave.open(wav_file, 'rb') as wf:
-        audio_data = wf.readframes(-1)
-        audio_data = np.frombuffer(audio_data, dtype=np.int16)
+    # with open('cv01_dobryden.wav', 'rb') as f:
+    # # head
+    # data = f.read(4)
+    # print(data)
+    # A1 = struct.unpack('i', f.read(4))[0]
+    # print(A1)
 
-        # Plot the audio signal
-        plt.figure(figsize=(10, 4))
-        plt.plot(audio_data)
-        plt.title('Audio Signal')
-        plt.xlabel('Sample')
-        plt.ylabel('Amplitude')
-        plt.grid(True)
-        plt.show()
+    # Open the WAV file for binary reading
+    with open('cv01_dobryden.wav', 'rb') as f:
+        riff_identifier = f.read(4)
+        print(f"riff: {riff_identifier.decode()}")
+
+        file_size = struct.unpack('<I', f.read(4))[0]
+        print(f"A1: {file_size} bytes")
+
+        wave_format = f.read(4)
+        print(f"WAVE retezec: {wave_format.decode()}")
+
+        fmt_subchunk_header = f.read(4)
+        print(f"'fmt ' retezec: {fmt_subchunk_header.decode()}")
+
+        fmt_subchunk_size = struct.unpack('<I', f.read(4))[0]
+        print(f"AF: {fmt_subchunk_size} bytes")
+
+        audio_format = struct.unpack('<H', f.read(2))[0]
+        print(f"K: {audio_format}")
+
+        num_channels = struct.unpack('<H', f.read(2))[0]
+        print(f"C: {num_channels}")
+
+        sample_rate = struct.unpack('<I', f.read(4))[0]
+        print(f"VF: {sample_rate} Hz")
+
+        byte_rate = struct.unpack('<I', f.read(4))[0]
+        print(f"PB: {byte_rate} bytes/second")
+
+        block_align = struct.unpack('<H', f.read(2))[0]
+        print(f"VB: {block_align} bytes")
+
+        bits_per_sample = struct.unpack('<H', f.read(2))[0]
+        print(f"VV: {bits_per_sample} bits")
+
+        data_subchunk_header = f.read(4)
+        print(f"'retezec data': {data_subchunk_header.decode()}")
+
+        data_subchunk_size = struct.unpack('<I', f.read(4))[0]
+        print(f"A2: {data_subchunk_size} bytes")
+
+        audio_data = np.frombuffer(f.read(data_subchunk_size), dtype=np.uint8)
+
+    # delka audia
+    audio_duration = len(audio_data) / (sample_rate * num_channels * (bits_per_sample // 8))
+
+    t = np.linspace(0, audio_duration, len(audio_data))
+
+    # Plot the audio signal
+    plt.figure(figsize=(10, 4))
+    plt.plot(t, audio_data)
+    plt.title('Audio Signal')
+    plt.xlabel('t[s]')
+    plt.ylabel('A[-]')
+    plt.grid(True)
+    plt.show()
 
 
 def plot_spectogram(wav_file):
@@ -44,7 +97,7 @@ def plot_spectogram(wav_file):
 
 
 if __name__ == "__main__":
-    my_wav_file = "audi.wav"  # Replace with the path to your .wav file
+    my_wav_file = "cv01_dobryden.wav"  # Replace with the path to your .wav file
     read_wav_header(my_wav_file)
     plot_wav_file(my_wav_file)
-    plot_spectogram(my_wav_file)
+    #plot_spectogram(my_wav_file)
