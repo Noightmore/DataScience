8148b489fe2830faf6c71e0524486c205c02ab4e
john.doe <robert.hak@tul.cz>
2023-11-30 22:49:56 +0100

    grays code???

--- multimedia/cv11/grayscode.py
+++ multimedia/cv11/grayscode.py
@@ -1,7 +1,32 @@
 
+def from_binary_to_gray(binary):
+    """Converts binary number to Gray code."""
+    gray = binary[0]
+    for i in range(1, len(binary)):
+        gray += str(int(binary[i - 1]) ^ int(binary[i]))
+    return gray
+
+
+def from_gray_to_binary(gray):
+    """Converts Gray code to binary number."""
+    binary = gray[0]
+    for i in range(1, len(gray)):
+        binary += str(int(binary[i - 1]) ^ int(gray[i]))
+    return binary
+
 
 def main():
-    pass
+    my_number = 123
+
+    binary = bin(my_number)[2:]
+    gray = from_binary_to_gray(binary)
+    print('Binary number: {}\nGray code: {}'.format(binary, gray))
+    print()
+    print()
+
+    binary = from_gray_to_binary(gray)
+    binary = int(binary, 2)
+    print('Gray code: {}\nBinary number: {}'.format(gray, binary))
 
 
 if __name__ == '__main__':
