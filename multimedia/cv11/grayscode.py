20fd9c97c671ce5da95925257669ae56270be286
john.doe <robert.hak@tul.cz>
2023-11-30 22:55:00 +0100

    grays code ok ok

--- multimedia/cv11/grayscode.py
+++ multimedia/cv11/grayscode.py
@@ -1,32 +1,28 @@
-
 def from_binary_to_gray(binary):
     """Converts binary number to Gray code."""
-    gray = binary[0]
-    for i in range(1, len(binary)):
-        gray += str(int(binary[i - 1]) ^ int(binary[i]))
+    gray = binary ^ (binary >> 1)
     return gray
 
 
 def from_gray_to_binary(gray):
     """Converts Gray code to binary number."""
-    binary = gray[0]
-    for i in range(1, len(gray)):
-        binary += str(int(binary[i - 1]) ^ int(gray[i]))
+    binary = gray
+    while gray >> 1:
+        gray >>= 1
+        binary ^= gray
     return binary
 
 
 def main():
     my_number = 123
 
-    binary = bin(my_number)[2:]
+    binary = my_number
     gray = from_binary_to_gray(binary)
-    print('Binary number: {}\nGray code: {}'.format(binary, gray))
-    print()
+    print('Decimal number: {}\nGray code: {}'.format(binary, gray))
     print()
 
     binary = from_gray_to_binary(gray)
-    binary = int(binary, 2)
-    print('Gray code: {}\nBinary number: {}'.format(gray, binary))
+    print('Gray code: {}\nDecimal number: {}'.format(gray, binary))
 
 
 if __name__ == '__main__':
