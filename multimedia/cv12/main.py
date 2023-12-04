a3644cdf726669e66eae95326ac08e1b655f15ac
robert <robert.hak@tul.cz>
2023-12-04 15:05:09 +0100

    bruh sus

---
+++ multimedia/cv12/main.py
@@ -0,0 +1,20 @@
+from collections import Counter
+import numpy as np
+
+
+def repair_inverse_code(message):
+    pass
+
+
+def main():
+    xor = lambda a, b: a ^ b
+
+    print(xor(1, 1))
+    print(xor(1, 0))
+    print(xor(0, 1))
+    print(xor(0, 0))
+
+
+
+if __name__ == "__main__":
+    main()
